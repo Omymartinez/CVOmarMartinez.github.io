0ad9d2035592001d810ae41558a3dc5f4138b9da
Omymartinez <124005285+Omymartinez@users.noreply.github.com>
2023-01-30 20:37:40 -0300

    Add files via upload

---
+++ CVOmarMartinez/function.js
@@ -0,0 +1,12 @@
+
+document.getElementById('primarios').addEventListener('click', function() {
+    document.getElementById("estudios").innerHTML = "Escuela Nº 3 Bernardino Rivadavia";
+});
+
+document.getElementById('secundarios').addEventListener('click', function() {
+    document.getElementById("estudios").innerHTML = "PCI AC FAA - Título Períto Mercantil";
+});
+
+document.getElementById('profesionales').addEventListener('click', function() {
+    document.getElementById("estudios").innerHTML = "ESFAE - Especialidad PAN";
+});
